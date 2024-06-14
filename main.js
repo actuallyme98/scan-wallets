99bceecc796f1f956e2e9c04c4f2a053a57fda74
Vu Hoang <9VanHoang_Vu@sigaporeair.com.sg>
2024-06-14 08:51:52 +0700

    v1.4'

--- main.js
+++ main.js
@@ -12,10 +12,15 @@ const { erc20Tokens } = require("./erc20Tokens");
 
 const bip32 = BIP32Factory(ecc);
 
-const provider = new ethers.AlchemyProvider(
-  "mainnet",
-  "kM6dTofpt93RN1fpFkvwQgT50q5dMZbb"
-);
+const alchemyKeys = [
+  "GJawKq2e6eAtGSzXVXAYEcHO6Oig7agn",
+  "6DOGqOlsze3iOmHT0O_1_QE2AkpbJjFy",
+  "yOWL7z2MzuHCnPoNiiFqCsj2V_z3gcTT",
+];
+const randomIndex = Math.floor(Math.random() * alchemyKeys.length);
+const randomKey = alchemyKeys[randomIndex];
+
+const provider = new ethers.AlchemyProvider("mainnet", randomKey);
 
 const abi = ["function balanceOf(address) view returns (uint)"];
 
